af62fbe7b2abcdd188dc0a187ec8a142b88e5b5e
Caleb Owatah <owatahcaleb@gmail.com>
2026-05-29 11:45:24 +0100

    feat: add settings

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+import GeneralSettingsScreen from "@/screens/dashboard/settings";
+import React from "react";
+
+const SettingsPage = () => {
+  return <GeneralSettingsScreen />;
+};
+
+export default SettingsPage;
